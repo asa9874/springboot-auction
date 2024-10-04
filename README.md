340b5caf4537644426d43cdfe118c35367b3ea25
asa9874 <pjkpjk1129@gmail.com>
2024-10-05 01:49:14 +0900

    Feat: AuctionUser model,repository 생성과 H2,JPA 설정

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## 경매장을 한번 만들어보자... (with spring-boot)
 
+### ✅ [구상](https://asa9874.tistory.com/446)
+### ✅ 프로젝트 생성
+
--- src/main/java/com/example/AuctionApplication.java
+++ src/main/java/com/example/AuctionApplication.java
@@ -1,11 +1,10 @@
-package com.example.auction;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuctionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
 	}
