1ed09392e6f5f0d7ebb75258321d5b0a48887d5d
asa9874 <pjkpjk1129@gmail.com>
2024-10-05 19:01:01 +0900

    Feat: Item Model,Repository 생성

--- src/main/java/com/example/model/Item.java
+++ src/main/java/com/example/model/Item.java
@@ -5,26 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-public class AuctionUser {
+public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique=true)
     private String name;
 
-    private String password;
-
-    @Column(unique=true)
-    private String email;
-
-    private Integer money;
-
+    @Column(length = 200)
+    private String Description;
 }
