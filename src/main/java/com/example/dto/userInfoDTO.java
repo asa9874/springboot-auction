1043ab158312b64e86d0f44db13fc646bca2cdda
asa9874 <pjkpjk1129@gmail.com>
2024-10-29 21:27:56 +0900

    Feat: userInfoDTO

---
+++ src/main/java/com/example/dto/userInfoDTO.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class userInfoDTO {
+    private Long userNickname;
+
+    private String email;
+
+    private Integer money;
+}
